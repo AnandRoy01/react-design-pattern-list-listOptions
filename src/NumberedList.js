c68e91f468d419996523458f27f8681385858974
nineleaps <nineleaps@nineleaps-admins-MacBook-Pro.local>
2021-12-07 12:37:26 +0530

    list pattern

---
+++ src/NumberedList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NumberedList = ({
+  items,
+  resourceName,
+  itemComponent: ItemComponent,
+}) => {
+  return (
+    <>
+      {items.map((item, idx) => (
+        <div key={idx}>
+          <h3>{idx + 1}</h3>
+          <ItemComponent {...{ [resourceName]: item }} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default NumberedList;
